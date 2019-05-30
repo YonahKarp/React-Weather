9a43b5936c13c9c2fe1ce5a538331286ec73fe00
Yonah Karp <ykarp@student.touro.edu>
2019-05-30 14:28:47 -0400

    Updates

--- src.a2b27638.js
+++ src.a2b27638.js
@@ -31616,7 +31616,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63174" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64117" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
